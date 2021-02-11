3a321dda91a03dd87edd3c00f792cb29251ae607
sota.yamamoto <yamasou23@gmail.com>
2021-02-12 00:36:29 +0900

    サインアップやログインのためのページ作成と, Firebase Auth機能の連携を実装

---
+++ lib/login.dart
@@ -0,0 +1,73 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class MyLoginPage extends StatefulWidget {
+  @override
+  _MyLoginPageState createState() => _MyLoginPageState();
+}
+
+class _MyLoginPageState extends State<MyLoginPage> {
+  String loginUserEmail = "";
+  String loginUserPassword = "";
+  String infoText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32.0),
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(30.0)),
+              TextFormField(
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    loginUserEmail = value;
+                  });
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード（８文字以上）"),
+                //　パスワードを見えないように
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    loginUserPassword = value;
+                  });
+                },
+              ),
+              Padding(padding: EdgeInsets.all(30.0)),
+              RaisedButton(
+                onPressed: () async {
+                  try {
+                    // メールとパスワードでユーザー登録
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    await auth.signInWithEmailAndPassword(
+                      email: loginUserEmail, password: loginUserPassword,
+                    );
+
+                    // 登録後Home画面に遷移
+                    await Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => MyHomePage(title: "My_Gaman_App")),
+                    );
+                  } catch (e) {
+                    // 登録に失敗した場合
+                    setState(() {
+                      infoText = "登録NG：{e.message}";
+                    });
+                  }
+                },
+                child: Text("Login"),
+              ),
+              Text(infoText)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/signup.dart
@@ -0,0 +1,73 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class MyAuthPage extends StatefulWidget {
+  @override
+  _MyAuthPageState createState() => _MyAuthPageState();
+}
+
+class _MyAuthPageState extends State<MyAuthPage> {
+  String newUserEmail = "";
+  String newUserPassword = "";
+  String infoText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32.0),
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(30.0)),
+              TextFormField(
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserEmail = value;
+                  });
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード（８文字以上）"),
+                //　パスワードを見えないように
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              Padding(padding: EdgeInsets.all(30.0)),
+              RaisedButton(
+                onPressed: () async {
+                  try {
+                    // メールとパスワードでユーザー登録
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    await auth.createUserWithEmailAndPassword(
+                      email: newUserEmail, password: newUserPassword,
+                    );
+
+                    // 登録後Home画面に遷移
+                    await Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => MyHomePage(title: "My_Gaman_App")),
+                    );
+                  } catch (e) {
+                    // 登録に失敗した場合
+                    setState(() {
+                      infoText = "登録NG：{e.message}";
+                    });
+                  }
+                },
+                child: Text("SignUp"),
+              ),
+              Text(infoText)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
